2aa2e567272187e26e162644104c927eb088623f
Wahedul Islam <wahedulislam.pro@gmail.com>
2026-02-01 20:56:06 +0600

    fix auto rendraing

--- src/app/meals/[id]/page.tsx
+++ src/app/meals/[id]/page.tsx
@@ -14,10 +14,18 @@ const MealDetailsPage = async ({
     notFound();
   }
 
+  console.log("Fetched meal:", meal); // 🔍 একবারই log হবে
+
   return (
     <div className="max-w-4xl mx-auto p-6">
+      <img
+        src={meal.image}
+        alt={meal.name}
+        className="w-full h-64 object-cover rounded mb-4"
+      />
+
       <h1 className="text-3xl font-bold">{meal.name}</h1>
-      <p>{meal.description}</p>
+      <p className="text-gray-600">{meal.description}</p>
       <p className="font-bold mt-2">৳ {meal.price}</p>
     </div>
   );
