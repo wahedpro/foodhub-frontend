e38f8e2e5ae006be2fc68d96ab1688f559047c1e
Wahedul Islam <wahedulislam.pro@gmail.com>
2026-02-03 19:26:20 +0600

    refactor: update layout width in Navbar and remove unused sections in Footer

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
 
   return (
     <nav className="sticky top-0 z-50 h-15 w-full bg-white">
-      <div className="mx-auto flex h-full md:w-[80%] items-center justify-between px-6 md:px-16 lg:px-24">
+      <div className="mx-auto flex h-full w-[95%] items-center justify-between px-6 md:px-16 lg:px-24">
         
         {/* Logo */}
         <Link href="/" className="text-xl font-bold text-[#e10101]">
